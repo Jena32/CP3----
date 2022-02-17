c44c11e3d355d9c9cd2771f8de43968234235c74
Jena32 <99413530+Jena32@users.noreply.github.com>
2022-02-17 23:08:19 +0700

    Create Exercise4_Jenjira_K.py

---
+++ Exercise4_Jenjira_K.py
@@ -0,0 +1,9 @@
+FE = 40.5
+GB = 67.2
+ICS = 64
+CP = 30.7
+print("--- Your Score ---")
+print("Foundation English : " , FE)
+print("General Business : " , GB)
+print("Introduction to Computer Systems : " , ICS)
+print("Computer Programming : " , CP)
