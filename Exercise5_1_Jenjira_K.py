a9e8a199035c2eea3cdec6946fd4bae2df98e530
Jena32 <99413530+Jena32@users.noreply.github.com>
2022-02-21 21:39:22 +0700

    Create Exercise5_1_Jenjira_K.py

---
+++ Exercise5_1_Jenjira_K.py
@@ -0,0 +1,10 @@
+n1 = int(input("First number :"))
+n2 = int(input("Second number :"))
+r1 = n1+n2
+r2 = n1-n2
+r3 = n1*n2
+r4 = n1/n2
+print(n1,"+",n2,"=",r1)
+print(n1,"-",n2,"=",r2)
+print(n1,"*",n2,"=",r3)
+print(n1,"/",n2,"=",r4)
