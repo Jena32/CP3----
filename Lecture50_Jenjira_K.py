633cd686c8b91913ce878aa7ceb1056438349f21
Jena32 <99413530+Jena32@users.noreply.github.com>
2022-03-29 22:07:52 +0700

    Create Lecture50_Jenjira_K.py

---
+++ Lecture50_Jenjira_K.py
@@ -0,0 +1,18 @@
+def addNumber(x,y):
+    print(x,"+",y,"=",x+y)
+
+def miNumber(x,y):
+    print(x,"-",y,"=",x-y)
+
+def mulNumber(x,y):
+    print(x,"*",y,"=",x*y)
+
+def diNumber(x,y):
+    print(x,"/",y,"=",x/y)
+
+x = int(input("Number1 = "))
+y = int(input("Number2 = "))
+addNumber(x,y)
+miNumber(x,y)
+mulNumber(x,y)
+diNumber(x,y)
